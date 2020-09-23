4a234ea1362b15fadb6856ee785ed767bef61426
yui <ssuji@mit.edu>
2020-09-23 18:05:50 -0400

    Add forward Euler code

---
+++ ForwardEuler.m
@@ -0,0 +1,28 @@
+function X = ForwardEuler(P, eval_f, x_start, p, eval_u, t_start, t_stop, timestep)
+% uses Forward Euler to simulate states model dx/dt=f(x,p,u)
+% from state x_start at time t_start
+% until time t_stop, with time intervals timestep
+% eval_f is a string including the name of the function that evaluates f(x,p,u)
+% eval_u is a string including the name of the funciton that evaluates u(t)
+% 
+% X = ForwardEuler(eval_f, x_start, p, eval_u, t_start, t_stop, timestep)
+
+% copyright Luca Daniel, MIT 2018
+
+X(:,1) = x_start;
+t(1) = t_start;
+
+
+for n=1:ceil((t_stop - t_start) / timestep) 
+   dt = min(timestep, (t_stop - t(n)));
+   t(n+1)= t(n) + dt;
+   u = feval(eval_u, P, t(n));
+%    f = cell2mat(feval(eval_f, X(:,:,n), p, u));
+   
+   %f = transpose(reshape(cell2mat(feval(eval_f, X(:,n), p, u)), [4, P]));
+   f = feval(eval_f, X(:,n), p, u);
+   
+   %X(:,n+1)= X(:,n) +  dt * f;
+   dt_time_f = cellfun(@(a) a*dt, f, 'UniformOutput', false);
+   X(:,n+1)= cellfun(@(b, c) b + c, X(:,n), dt_time_f, 'UniformOutput', false);
+end
