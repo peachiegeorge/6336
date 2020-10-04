f5d508df4aefe7026702ecf7b032319e126433c1
Jason-SURFACE <jzhang395@gmail.com>
2020-10-04 18:34:41 -0400

    Add comments to LINEARIZEF

--- LINEARIZEF.m
+++ LINEARIZEF.m
@@ -1,12 +1,15 @@
 function [A,B] = LINEARIZEF(f,x0,p,u0,epsX,epsU,doScalarEval)
-% f : P x 1 cell array of outputs {[dS1/dt; dE1/dt; dI1/dt; dR1/dt];...
-%                                 [dSP/dt; dEP/dt; dIP/dt; dRP/dt]}
-% x0 : P x 1 cell array of state vector inputs {[S1; E1; I1; R1]; [SP; EP; IP; RP]}
-%      about which to linearize
-% p  : P x 1 struct of params
-% u0 : P x 1 cell array of inputs {[U1; U1; U1; U1];...
-%                                 [UP; UP; UP; UP]}
-% u  : 
+% TO RUN: Provide 
+% A  : Jacobian w.r.t state variables, x, evaluated at operating pt. x0
+% B  : K0 (see def. in PM2 doc) and Jf_u, the Jcobian w.r.t inputs/sources u
+% f  : function handler, e.g. f = 'EVALF' or f = @(x) x.^2;
+% x0 : state vector about which to evaluate the Jacobian
+% p  : Parameter struct
+% u0 : input vector about which to evaluate the Jacbobian
+% epsX : small epsilon in x ('deltaX')
+% epsU : small epsilon in u ('deltaU');
+% doScalarEval : a flag to let the functions know if you are evaluating a
+%                scalar function
 
 [Jf_u, Jf_x] = finiteDifferenceJacobian(f,x0,p,u0,epsX,epsU,doScalarEval);
 if doScalarEval
