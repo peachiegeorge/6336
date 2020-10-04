36f8a99b457e57b1ef4a6b2c257405ab6cc6443c
Jason-SURFACE <jzhang395@gmail.com>
2020-10-04 16:27:19 -0400

    Modify Jacobian to compute w.r.t. x in addition to u

--- finiteDifferenceJacobian.m
+++ finiteDifferenceJacobian.m
@@ -1,23 +1,29 @@
-function[Jf] = finiteDifferenceJacobian()
+function[Jf_u, Jf_x] = finiteDifferenceJacobian()
 
 P = 100;    % # nodes simulated
 theta = GenThetaMat(P, 'symmetric');
-x = GenStateVec(P, 'else');
+x = GenStateVec(P, 'else'); % Operating point is about 0
 p = GenPStruct(P,theta);
 u = GenInputVec(P, 0); % t = 0 input 
-epsilon = 0.01;
+epsilonX = 1;
+epsilonU = 1;
 
 numNodes = size(x,1);
 numEquations= size(x,1)*4;
 
-Jf = zeros(numEquations,numEquations);
+Jf_u = zeros(numEquations,numEquations);
+Jf_x = zeros(numEquations,numEquations);
 
 for ind = 1:numNodes
     for eq = 1:4
         uStep = u;
-        uStep{ind}(eq) = uStep{ind}(eq) + epsilon;
-        fDiff = cellfun(@minus, EVALF(x,p,uStep), EVALF(x,p,u),'Un',0);
-        Jf((ind-1)*4+eq,:) = (1/epsilon) * cell2mat(fDiff);
+        uStep{ind}(eq) = uStep{ind}(eq) + epsilonU;
+        xStep = x;
+        xStep{ind}(eq) = x{ind}(eq) + epsilonX;
+        fDiffU = cellfun(@minus, EVALF(x,p,uStep), EVALF(x,p,u),'Un',0);
+        fDiffX = cellfun(@minus, EVALF(xStep,p,u), EVALF(x,p,u),'Un',0);
+        Jf_u((ind-1)*4+eq,:) = (1/epsilonU) * cell2mat(fDiffU);
+        Jf_x((ind-1)*4+eq,:) = (1/epsilonX) * cell2mat(fDiffX);
         %disp(Jf(:,(ind-1)*4+eq));
     end
 end
