1ae52dd60460b2168120094991820564a347e3e6
Jason-SURFACE <jzhang395@gmail.com>
2020-10-04 18:22:25 -0400

    Modify LINEARIZEF and finiteDifferenceJacobian to take in scalar and vector functions; kind of working

--- LINEARIZEF.m
+++ LINEARIZEF.m
@@ -1,4 +1,4 @@
-function [A,B] = LINEARIZEF(x0,p,u0)
+function [A,B] = LINEARIZEF(f,x0,p,u0,epsX,epsU,doScalarEval)
 % f : P x 1 cell array of outputs {[dS1/dt; dE1/dt; dI1/dt; dR1/dt];...
 %                                 [dSP/dt; dEP/dt; dIP/dt; dRP/dt]}
 % x0 : P x 1 cell array of state vector inputs {[S1; E1; I1; R1]; [SP; EP; IP; RP]}
@@ -8,8 +8,12 @@ function [A,B] = LINEARIZEF(x0,p,u0)
 %                                 [UP; UP; UP; UP]}
 % u  : 
 
-[Jf_u, Jf_x] = finiteDifferenceJacobian(x0,p,u0);
-K0 = cell2mat(EVALF(x0,p,u0)) - Jf_x*cell2mat(x0) - Jf_u*cell2mat(u0);
+[Jf_u, Jf_x] = finiteDifferenceJacobian(f,x0,p,u0,epsX,epsU,doScalarEval);
+if doScalarEval
+    K0 = feval(f,x0) - Jf_x*x0;
+else
+    K0 = cell2mat(feval(f,x0,p,u0)) - Jf_x*cell2mat(x0) - Jf_u*cell2mat(u0);
+end
 A = Jf_x;
 B = [K0,Jf_u];
 
--- finiteDifferenceJacobian.m
+++ finiteDifferenceJacobian.m
@@ -1,24 +1,44 @@
-function[Jf_u, Jf_x] = finiteDifferenceJacobian(x,p,u)
-epsilonX = 1;
-epsilonU = 1;
+function[Jf_u, Jf_x] = finiteDifferenceJacobian(f,x,p,u,epsX,epsU,doScalarEval)
 
 numNodes = size(x,1);
-numEquations= size(x,1)*4;
+if iscell(x)
+    numEquations= size(x,1)*size(x{1},1);
+else
+    numEquations = size(x,1);
+end
 
 Jf_u = zeros(numEquations,numEquations);
 Jf_x = zeros(numEquations,numEquations);
 
 for ind = 1:numNodes
-    for eq = 1:4
+    for eq = 1:numEquations
         uStep = u;
-        uStep{ind}(eq) = u{ind}(eq) + epsilonU;
+        if size(u) > 1
+            % Vector
+            uStep{ind}(eq) = u{ind}(eq) + epsU;
+        else
+            % Scalar
+            uStep = u + epsU;
+        end
         xStep = x;
-        xStep{ind}(eq) = x{ind}(eq) + epsilonX;
-        fDiffU = cellfun(@minus, EVALF(x,p,uStep), EVALF(x,p,u),'Un',0);
-        fDiffX = cellfun(@minus, EVALF(xStep,p,u), EVALF(x,p,u),'Un',0);
-        Jf_u((ind-1)*4+eq,:) = (1/epsilonU) * cell2mat(fDiffU);
-        Jf_x((ind-1)*4+eq,:) = (1/epsilonX) * cell2mat(fDiffX);
-        %disp(Jf(:,(ind-1)*4+eq));
+        if size(x) > 1
+            % Vector
+            xStep{ind}(eq) = x{ind}(eq) + epsX;
+        else
+            % Scalar
+            xStep = x + epsX;
+        end
+        if doScalarEval
+            fDiffX = feval(f,xStep) - feval(f,x);
+            Jf_x = (1/epsX)*fDiffX;
+            Jf_u = [];
+        else
+            fDiffU = cellfun(@minus, feval(f,x,p,uStep), feval(f,x,p,u), 'Un',0);
+            fDiffX = cellfun(@minus, feval(f,xStep,p,u), feval(f,x,p,u), 'Un',0);
+            Jf_u((ind-1)*4+eq,:) = (1/epsU) * cell2mat(fDiffU);
+            Jf_x((ind-1)*4+eq,:) = (1/epsX) * cell2mat(fDiffX);
+        end
+
     end
 end
 end
