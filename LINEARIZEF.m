158f4a219312f24f508aed8a6555cefb1589b54e
Jason-SURFACE <jzhang395@gmail.com>
2020-10-04 17:35:25 -0400

    Update LINEARIZEF; should be working but not tested

--- LINEARIZEF.m
+++ LINEARIZEF.m
@@ -1,4 +1,4 @@
-function [A,B] = LINEARIZEF(f,x0,p,u0,u)
+function [A,B] = LINEARIZEF(x0,p,u0)
 % f : P x 1 cell array of outputs {[dS1/dt; dE1/dt; dI1/dt; dR1/dt];...
 %                                 [dSP/dt; dEP/dt; dIP/dt; dRP/dt]}
 % x0 : P x 1 cell array of state vector inputs {[S1; E1; I1; R1]; [SP; EP; IP; RP]}
@@ -8,5 +8,9 @@ function [A,B] = LINEARIZEF(f,x0,p,u0,u)
 %                                 [UP; UP; UP; UP]}
 % u  : 
 
+[Jf_u, Jf_x] = finiteDifferenceJacobian(x0,p,u0);
+K0 = cell2mat(EVALF(x0,p,u0)) - Jf_x*cell2mat(x0) - Jf_u*cell2mat(u0);
+A = Jf_x;
+B = [K0,Jf_u];
 
 end
