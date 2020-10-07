10cc5ff68ad362000c2b51f08d719ad784d136d9
Jason <jnzhang@MIT.edu>
2020-10-07 16:09:32 -0400

    Add small leakage to EVALF.m; keeps it from going singular

--- GenPStruct.m
+++ GenPStruct.m
@@ -8,6 +8,7 @@ p(P, 1).beta = 0;  % Initialize struct array
 for i = 1:P
     p(i,1).beta = 0.009;
     p(i,1).sigma = 0.1;
-    p(i,1).gamma = 0.1;
+    p(i,1).gamma = 0.5;
+    p(i,1).nu = 0.1;
     p(i,1).theta = theta(i,:); % Extract row for that node
 end
