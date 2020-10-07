7b9e5253847c201edaa80d78bc288211142a9c7c
yui <ssuji@mit.edu>
2020-10-07 13:12:12 -0400

    WIP for Ax=b with singular matrix error

--- testSolveSteadyState.m
+++ testSolveSteadyState.m
@@ -7,10 +7,16 @@ function [L, U, Pe, X, Y] = testSolveSteadyState()
 %         4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4]);
 %     
 %     [L, U, Pe, X, Y] = solveSteadyState(A, B);
-    
+
+    % Generate Random Numbers That Are Repeatable
+    rng('default');
+    rng(2);
+
     P = 4;
 
-    theta = GenThetaMat(P, 'symmetric');
+    %theta = GenThetaMat(P, 'symmetric');
+    theta = zeros(P, P)
+    
     x = GenStateVec(P, '');
     p = GenPStruct(P,  theta);
     u = GenInputVec(P, 0);
@@ -19,15 +25,71 @@ function [L, U, Pe, X, Y] = testSolveSteadyState()
     
     A = analyticJacobian(P, x, p, u, f, theta);
     
-    num_excitations = 2
+    num_excitations = 1
     B = zeros(4*P, num_excitations)
     
     for i = 1:num_excitations
-        u = GenInputVec(P, i);     
+        %u = GenInputVec(P, 0);     
         f = EVALF(x, p, u);
-        B(:, i) = cell2mat(f)
+        B(:, i) = reshape(-cell2mat(f), [4*P, 1])
     end
     
     [L, U, Pe, X, Y] = solveSteadyState(A, B);    
     
 end
+
+% function A = GenMatrixA(P, p)
+% 
+%     A = zeros(4*P, 4*P)
+%     theta = GenLinearThetaMat(P)
+%     p = GenLinearPStruct(P, theta)
+%     
+%     for i = 1:P % For each node
+%         
+%         A(i,i) = - p(i).beta;
+%         
+%         for j = 1:P
+%            if i ~= j
+%                A(i,j) = p(i).theta(i,j)
+%            else
+%                % Row of S
+%                if mod(i, 4) == 1
+%                    
+%                % Row of E
+%                elseif mod(i, 4) == 2
+%                
+%                % Row of I
+%                elseif mod(i, 4) == 3
+%                
+%                % Row of R
+%                elseif mod(i, 4) == 0
+%                    
+%                end
+%            end
+%         end
+%         
+%         p(i).theta;
+%         p(i).sigma;
+%         p(i).gamma;
+%     end
+% end
+% 
+% function theta = GenLinearThetaMat(P)
+% % Function to generate matrix of thetas given # of nodes
+% % P: total number of nodes
+% % theta: P x P matrix of theta parameters
+% theta = zeros(P, P);
+% 
+% function p = GenLinearPStruct(P, theta)
+% % Function to generate struct of parameters
+% % Set parameters for each node
+% % P: total number of nodes
+% % theta: matrix of thetas
+% % p: P x 1 cell array
+% p(P, 1).beta = 0;  % Initialize struct array
+% for i = 1:P
+%     p(i,1).beta = 0.009;
+%     p(i,1).sigma = 0.1;
+%     p(i,1).gamma = 0.1;
+%     p(i,1).theta = theta(i,:); % Extract row for that node
+% end
