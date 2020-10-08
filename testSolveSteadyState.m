c17d98ea2c21afa5e85bab35a861c8c1fd8d4744
yui <ssuji@mit.edu>
2020-10-07 23:23:51 -0400

    Adjust parameter values to match PM2 test case in part E document

--- testSolveSteadyState.m
+++ testSolveSteadyState.m
@@ -10,14 +10,15 @@ function [L, U, Pe, X, Y] = testSolveSteadyState()
     theta = zeros(P, P)
     
     x = GenSteadyStateVec(P, 1)
-    p = GenPStruct(P, theta);
+    %p = GenPStruct(P, theta);
+    p = GenSteadyPStruct(P, theta);
     
     % value in u is not used but EVALF require u as a parameter
     u = GenInputVec(P, 1);
     
     A = transpose(analyticJacobian(P, x, p, theta));
     
-    num_excitations = 100
+    num_excitations = 10
     B = zeros(4*P, num_excitations)
     
     for i = 1:num_excitations
@@ -42,3 +43,38 @@ function x = GenSteadyStateVec(P, seed)
     x{3} = [7200; rand_e(3); 620; rand_r(3)];
     x{4} = [3400; rand_e(4); 860; rand_r(4)];
 end
+
+function p = GenSteadyPStruct(P, theta)
+% Function to generate struct of parameters
+% Set parameters for each node
+% P: total number of nodes
+% theta: matrix of thetas
+% p: P x 1 cell array
+p(P, 1).beta = 0;  % Initialize struct array
+
+p(1,1).beta = 0.04;
+p(1,1).sigma = 0.2;
+p(1,1).gamma = 0.5;
+p(1,1).nu = 0.1;
+p(1,1).theta = theta(1,:); % Extract row for that node
+
+p(2,1).beta = 0.05;
+p(2,1).sigma = 0.3;
+p(2,1).gamma = 0.4;
+p(2,1).nu = 0.2;
+p(2,1).theta = theta(2,:); % Extract row for that node
+
+p(3,1).beta = 0.01;
+p(3,1).sigma = 0.1;
+p(3,1).gamma = 0.3;
+p(3,1).nu = 0.3;
+p(3,1).theta = theta(3,:); % Extract row for that node
+
+p(4,1).beta = 0.01;
+p(4,1).sigma = 0.3;
+p(4,1).gamma = 0.2;
+p(4,1).nu = 0.4;
+p(4,1).theta = theta(4,:); % Extract row for that node
+
+end
+
