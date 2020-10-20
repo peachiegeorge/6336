af78a9d2646aecd90a07ba049e17c727dea6eaeb
Jason <jnzhang@MIT.edu>
2020-10-20 13:01:20 -0400

    Modify GenThetaMat to generate more physical theta matrix

--- GenThetaMat.m
+++ GenThetaMat.m
@@ -4,25 +4,26 @@ function theta = GenThetaMat(P, method)
 % radiation model.
 % P: total number of nodes
 % Method: Method for generating thetas
-% theta: P x (P-1) matrix of theta parameters
+% theta: P x P matrix of theta parameters
 NORM_FACT = 50; % Maximum possible travel per unit time
 if method == "symmetric"
     d = rand(P,1);
     t = triu(bsxfun(@min,d,d.').*rand(P)/NORM_FACT,1); % The upper trianglar random values
-    theta = diag(d)+t-t.'; % Put them together in a symmetric matrix
+    theta = diag(d)+t+t.'; % Put them together in a symmetric matrix
     theta = theta - diag(diag(theta)); % Zero out the diagonals
 elseif method == "upTri"
-    d = 2*rand(P,1)-1;  % Generate random nums between [-1 1]
-    theta = triu(bsxfun(@min,d,d.').*(2*rand(P)-1)/NORM_FACT,1); % The upper trianglar random values
-elseif method == "rand"
-    theta = zeros(P,P);
-    for m = 1:P
-        for n = 1:P
-            if m ~= n
-                theta(m,n) = rand(1)/NORM_FACT;
-            end
-        end
-    end
+    d = 2*rand(P,P)-1;  % Generate random nums between [-1 1]
+    theta = triu(d./NORM_FACT,1); % The upper trianglar random values
+elseif method == "random"
+%     theta = zeros(P,P);
+%     for m = 1:P
+%         for n = 1:P
+%             if m ~= n
+%                 theta(m,n) = rand(1)/NORM_FACT;
+%             end
+%         end
+%     end
+    theta = randfixedsum(P,P,1,0,1)/NORM_FACT;
 elseif method == "zeroes"
     theta = zeros(P,P);
 end
