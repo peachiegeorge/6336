40b2001834358f56bd423a61e6bd87e5498675c4
yui <ssuji@mit.edu>
2020-11-05 08:14:59 -0500

    Limit Newton step size, tolerance condition on norm of x

--- Steady-State/GenSteadyStateInitialGuess.m
+++ Steady-State/GenSteadyStateInitialGuess.m
@@ -10,12 +10,12 @@ function x = GenSteadyStateInitialGuess(P)
 %     delta_x = transpose([-8517 718 76 1301 -8587 2274 862 2414 -25258 13500 1630 2220 -45682 24042 11191 5955]);
 %     x = convertSeirMatToCell(xMat - delta_x);
     
-%     [num_row, num_col] = size(x);
-%     for i=1:num_row
-%         for j=1:4
-%            x{i}(j) = x{i}(j)/2;
-%         end
-%     end
+    [num_row, num_col] = size(x);
+    for i=1:num_row
+        for j=1:4
+           x{i}(j) = x{i}(j)/2;
+        end
+    end
     
     
 end
--- newtonNdQ.m
+++ newtonNdQ.m
@@ -13,19 +13,18 @@ function x0 = newtonNdQ(x0, p, u, theta, q)
 % number of Newton steps that are plotted sequentially
 % pauses between sub-steps
 
-tol=10;          % convergence tolerance
-delta_tol=10;     % convergence tolerance
-delta_percent_tol = 0.1; % convergence tolerance
+f_tol=1;          % convergence tolerance
+delta_tol=1;     % convergence tolerance
+delta_percent_tol = 0.01; % convergence tolerance
+x_tol=1;
 
 maxIters=500;       % max # of iterations
 x00=x0;             % initial guess
 
-f = EVALFQ(x0, p, u, q); 
-J = analyticJacobianQ(x0, p, theta, q);
-dx=J\-f;
+[num_row, num_col] = size(x0);
+num_x0_nodes = num_row;
 
-first_nf = norm(f,2);
-first_ndx = norm(dx,2);
+first_nx = norm(convertSeirCellToMat(x0),2);
 
 % Newton loop
 for iter=1:maxIters
@@ -34,38 +33,68 @@ for iter=1:maxIters
     J = analyticJacobianQ(x0, p, theta, q);    % evaluate Jacobian
     
     %Generate preconditioner of J
-    T = genDiagPreconditioner(J);
-    
-    %Scale J by Preditioner
-    old_J = J;
-    J = T*J;  
-    
-    %Scale f by Preditioner
-    old_f = f;
-    f = T*f;        
+%     T = genDiagPreconditioner(J);
+%     
+%     %Scale J by Preditioner
+%     old_J = J;
+%     J = T*J;  
+%     
+%     %Scale f by Preditioner
+%     old_f = f;
+%     f = T*f;        
     
     dx=J\-f;
     
     nf(iter)=norm(f,2);          % norm of f at step k+1
     ndx(iter)=norm(dx,2);        % norm of dx at step k+1)
-    nx(iter)=norm(convertSeirCellToMat(x0),2);  % norm of x at step k+1)
+    
     
     if iter > 1
-        delta_percent = ndx(iter)/nx(iter-1); 
+        delta_percent(iter) = ndx(iter)/nx(iter-1); 
+    end
+    
+    if iter == 1
+        first_nf = nf(iter);
+    end
+    
+    %Scale dx to avoid overshooting     
+    old_nx = norm(convertSeirCellToMat(x0),2);
+    if ndx(iter) > old_nx
+        old_dx = dx;
+        old_ndx = ndx(iter);
+        old_dx = dx;
+        
+        dx = dx / ndx(iter) * 0.5 * old_nx;
+        ndx(iter)=norm(dx,2);
     end
     
     raw_x0 = convertSeirCellToMat(x0) + dx;     %x0 with full change in dx
-    capped_x0 = max(raw_x0, 0);                 %Cap each element of x0 to be non-negative
+    rounded_raw_x0 = round(raw_x0);
+    capped_x0 = round(max(raw_x0, 0));          %Cap each element of x0 to be non-negative
+                                                %and round to nearest integer
     
-    %x(:, iter)=convertSeirMatToCell(convertSeirCellToMat(x0) + dx);              % solution x at step k+1
+    %x(:, iter)=convertSeirMatToCell(rounded_raw_x0);              % solution x at step k+1
+    
     x(:, iter)=convertSeirMatToCell(capped_x0);              % solution x at step k+1
+    %x(:, iter)=convertSeirMatToCell(raw_x0);              % solution x at step k+1    
     
     x0=x(:, iter);                 % set value for next guess
-    if nf(iter) < tol,          % check for convergence
+    
+    nx(iter)=norm(convertSeirCellToMat(x0),2);  % norm of x at step k+1)
+    
+    if nf(iter) < f_tol,          % check for convergence
         % check for convergence
         fprintf('Converged in %d iterations\n',iter);
 %         disp(x0)
         fprintf('Reach ||f(x)|| tolerance\n');
+        break; 
+    end
+    
+    if nx(iter) < x_tol,          % check for convergence
+        % check for convergence
+        fprintf('Converged in %d iterations\n',iter);
+%         disp(x0)
+        fprintf('Reach || x || tolerance\n');
         break; 
     end
     
@@ -77,13 +106,13 @@ for iter=1:maxIters
         break;         
     end
 
-    if (iter > 1) && (delta_percent < delta_percent_tol)
+    if (iter > 1) && (delta_percent(iter) < delta_percent_tol)
         % check for convergence
         fprintf('Converged in %d iterations\n',iter);
 %         disp(x0)       
         fprintf('Reach (delta ||f(x)||) / ||x|| tolerance\n');
         break;         
-    end
+    end    
     
 end
 
@@ -91,18 +120,21 @@ if iter==maxIters, % check for non-convergence
     fprintf('Non-Convergence after %d iterations!!!\n',iter); 
 end
 
-% iters = [1:iter];
-% 
+all_nx = [first_nx, nx];
+all_x = [x00, x];
+
+iters = [1:iter];
+
 % figure(4);
-% semilogy([0,iters],[first_nf, nf],'*-'); grid on; 
+% semilogy([iters],[nf],'*-'); grid on; 
 % xlabel('iteration #'); ylabel('||f(x)||');
 % 
 % figure(5)
-% semilogy([0,iters],[first_ndx, ndx],'*-'); grid on; 
+% semilogy([iters],[ndx],'*-'); grid on; 
 % xlabel('iteration #'); ylabel('||dx||');
 % 
 % figure(6)
-% semilogy([0,iters],[first_nf, nf]/first_nf,'*-'); grid on; 
+% semilogy([iters],[nf]/first_nf,'*-'); grid on; 
 % xlabel('iteration #'); ylabel('||f(x_k)|| / ||f(x_0)||');
 % title('Standard Newton')
 
