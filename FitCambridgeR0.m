26dd0615dd4a89e7f87c53c5b11d03c58c0bdf69
Jason-Work <jnzhang@MIT.edu>
2020-11-25 16:11:51 -0500

    Add Cambridge data and FitCambridge

---
+++ FitCambridgeR0.m
@@ -0,0 +1,21 @@
+load CambridgeCOVID19
+%%
+figure;
+t = 1:268;	% Days
+plot(t,CambridgeCOVID19.NewPositiveCases);
+title('New Positive Cases');
+xlabel('Days');
+ylabel('# New Positives');
+
+figure;
+plot(t,CambridgeCOVID19.CumulativePositiveCases);
+title('Cumulative Positive Cases');
+xlabel('Days');
+ylabel('# Cumulative Positives');
+
+%% 
+figure;
+semilogy(t,CambridgeCOVID19.CumulativePositiveCases);
+title('Cumulative Positive Cases');
+xlabel('Days');
+ylabel('# Cumulative Positives');
