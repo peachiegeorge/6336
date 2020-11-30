96d17b60b45d71b39ec1a6cd70a7010f6b9f5488
Jason Zhang <jzhang395@gmail.com>
2020-11-30 10:23:56 -0500

    Reduce NORM_FACT in GenThetaMat

--- Helper-Funcs/GenThetaMat.m
+++ Helper-Funcs/GenThetaMat.m
@@ -5,8 +5,7 @@ function theta = GenThetaMat(P, method, cutOffVal)
 % P: total number of nodes
 % Method: Method for generating thetas
 % theta: P x P matrix of theta parameters
-NORM_FACT = 10000; % Maximum possible travel per unit time
-
+NORM_FACT = 1; % Maximum possible travel per unit time
 if method == "symmetric"
     d = rand(P,1);
     t = triu(bsxfun(@min,d,d.').*rand(P)/NORM_FACT,1); % The upper trianglar random values
@@ -16,61 +15,47 @@ elseif method == "upTri"
     d = 2*rand(P,P)-1;  % Generate random nums between [-1 1]
     theta = triu(d./NORM_FACT,1); % The upper trianglar random values
 elseif method == "random"
-%     theta = zeros(P,P);
-%     for m = 1:P
-%         for n = 1:P
-%             if m ~= n
-%                 theta(m,n) = rand(1)/NORM_FACT;
-%             end
-%         end
-%     end
     theta = randfixedsum(P,P,1,0,1)/NORM_FACT;
     theta(1:size(theta,2)+1:end) = 0;
 elseif method == "zeros"
     theta = zeros(P,P);
-    
 elseif method == "noMeasures"
-load('cambridgeParams.mat');
-for i = 1:P
-    for j = 1:P
-        theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
-        theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
-        theta(i,j) = max(theta1,theta2);
+    load('Cambridge\cambridgeParams.mat');
+    for i = 1:P
+        for j = 1:P
+            theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
+            theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
+            theta(i,j) = max(theta1,theta2);
+        end
     end
-end
-theta = theta(1:P,1:P);
-
+    theta = theta(1:P,1:P);
 elseif method == "cutOff"
-    
-load('cambridgeParams.mat');
-for i = 1:P
-    for j = 1:P
-        theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
-        theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
-        theta(i,j) = max(theta1,theta2);
+    load('Cambridge\cambridgeParams.mat');
+    for i = 1:P
+        for j = 1:P
+            theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
+            theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
+            theta(i,j) = max(theta1,theta2);
+        end
     end
-end
-theta(cutOffVal,:) = 0;
-theta(:,cutOffVal) = 0;
-theta = theta(1:P,1:P);
-
+    theta(cutOffVal,:) = 0;
+    theta(:,cutOffVal) = 0;
+    theta = theta(1:P,1:P);
 elseif method == "cutOffMultiple"
-    
-load('cambridgeParams.mat');
-for i = 1:P
-    for j = 1:P
-        theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
-        theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
-        theta(i,j) = max(theta1,theta2);
+    load('Cambridge\cambridgeParams.mat');
+    for i = 1:P
+        for j = 1:P
+            theta1 = jobs(i) * pop(i) * poverty(j) / NORM_FACT;
+            theta2 = jobs(j) * pop(j) * poverty(i) / NORM_FACT;
+            theta(i,j) = max(theta1,theta2);
+        end
     end
-end
-theta(1,:) = 0;
-theta(:,1) = 0;
-theta(2,:) = 0;
-theta(:,2) = 0;
-theta(5,:) = 0;
-theta(:,5) = 0;
-theta = theta(1:P,1:P);
-
+    theta(1,:) = 0;
+    theta(:,1) = 0;
+    theta(2,:) = 0;
+    theta(:,2) = 0;
+    theta(5,:) = 0;
+    theta(:,5) = 0;
+    theta = theta(1:P,1:P);
 end
 end
